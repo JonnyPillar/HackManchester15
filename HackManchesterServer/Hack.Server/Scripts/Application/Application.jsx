3bb5ac1649d19552ff4d733e9ae3a1ed79afa00c
Jonny Pillar <jonny@jonnypillar.co.uk>
2015-10-24 19:24:40 +0100

    giving up on the react and setting by VMC

--- HackManchesterServer/Hack.Server/Scripts/Application/Application.jsx
+++ HackManchesterServer/Hack.Server/Scripts/Application/Application.jsx
@@ -1,3 +1,29 @@
-﻿var temp = require ('./Home');
+﻿var react = require('react');
+var ReactDOM = require('react-dom');
 
-temp();
+var CheckLink = react.createElement({
+    render: function() {
+        // This takes any props passed to CheckLink and copies them to <a>
+
+        var temp = [1, 2, 3];
+
+        return(
+            <div>
+                Hello again
+                {
+                    temp.map(function(item){
+                        return(
+                            <b>{item}</b>
+                        );
+                    })
+                }
+            </div>
+        );
+    }
+});
+
+module.exports = function(){
+    return (ReactDOM.render(
+        CheckLink, document.getElementById('homePanel')
+    ));
+};

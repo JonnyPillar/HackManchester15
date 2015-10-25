3eed540b9482eac9a0c1094dbb9ff98a5b46632c
emmavsmith <emmasmith059@gmail.com>
2015-10-25 04:34:08 +0000

    Poll changes

---
+++ HackManchesterServer/Hack.Domain/DataContracts/ApiResponses/SubmitQuestionResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace Hack.Domain.DataContracts.ApiResponses
+{
+    public class SubmitQuestionResponse
+    {
+        public SubmitQuestionResponse(long questionId)
+        {
+            QuestionId = questionId;
+        }
+
+        public long QuestionId { get; set; }
+    }
+}

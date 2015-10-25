92ef0d85a0ca9809009b60d633a5cfd6774e41a2
Jonny Pillar <jonny@jonnypillar.co.uk>
2015-10-25 06:29:34 +0000

    sdvsd
    vsdv

--- HackManchesterServer/Hack.Server/Controllers/HomeController.cs
+++ HackManchesterServer/Hack.Server/Controllers/HomeController.cs
@@ -56,7 +56,7 @@ namespace Hack.Server.Controllers
         {
             Id = question.Id;
             Title = question.Title.Length > 39 ? question.Title.Substring(0, 40) + "..." : question.Title;
-            Description = question.Description.Length > 115 ? question.Description.Substring(0, 124) + "..." : question.Description;
+            Description = question.Description.Length > 115 ? question.Description.Substring(0, 115) + "..." : question.Description;
             if (question.QuestionTags.Count >= 1)
             {
                 ImageUrl = question.QuestionTags.First().ImageUrl;
